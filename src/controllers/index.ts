19a70e8617dc66a620a12c66dbd48b96751abafa
Mohamed Eid Latif <MohamedLatif@my.uopeople.edu>
2025-10-29 20:08:02 +0300

    feat: add controllers index

---
+++ src/controllers/index.ts
@@ -0,0 +1,8 @@
+// User Controllers
+export { register, login } from "./userController";
+
+// Product Controllers
+export { getAll, getById, create, update, remove } from "./productController";
+
+// Cart Controllers
+export { getCart, addItem } from "./cartController";
