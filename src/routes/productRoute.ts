5cca5e440dedb07f211b60da60d41e33c298ecc7
Mohamed Eid Latif <MohamedLatif@my.uopeople.edu>
2025-10-29 20:09:24 +0300

    Update product routes

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,59 +1,47 @@
 import express from "express";
 import {
-  getAllProducts,
-  getProductById,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-} from "../services/productService";
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+} from "../controllers/productController";
 
 const router = express.Router();
 
 /**
- *  GET /products
- *  Get all products
- *  access  Public
+ * GET /products
+ * Get all products
+ * @access Public
  */
-router.get("/", async (req, res) => {
-  const { data, statuscode } = await getAllProducts();
-  res.status(statuscode).json(data);
-});
-
-//  GET /products/:id
-
-router.get("/:id", async (req, res) => {
-  const { data, statuscode } = await getProductById(req.params.id);
-  res.status(statuscode).json(data);
-});
+router.get("/", getAll);
 
 /**
- *  POST /products
- *  Create a new product
- *  access  Private (should be restricted to admins)
+ * GET /products/:id
+ * Get a single product by ID
+ * @access Public
  */
-router.post("/", async (req, res) => {
-  const { data, statuscode } = await createProduct(req.body);
-  res.status(statuscode).json(data);
-});
+router.get("/:id", getById);
+
+/**
+ * POST /products
+ * Create a new product
+ * @access Private (should be restricted to admins)
+ */
+router.post("/", create);
 
 /**
  * PUT /products/:id
  * Update a product
- * access  Private (should be restricted to admins)
+ * @access Private (should be restricted to admins)
  */
-router.put("/:id", async (req, res) => {
-  const { data, statuscode } = await updateProduct(req.params.id, req.body);
-  res.status(statuscode).json(data);
-});
+router.put("/:id", update);
 
 /**
  * DELETE /products/:id
  * Delete a product
- * access  Private (should be restricted to admins)
+ * @access Private (should be restricted to admins)
  */
-router.delete("/:id", async (req, res) => {
-  const { data, statuscode } = await deleteProduct(req.params.id);
-  res.status(statuscode).json(data);
-});
+router.delete("/:id", remove);
 
 export default router;
