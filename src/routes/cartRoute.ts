c631a77b3d93e70903c8cee0717895f99352966a
Mohamed Eid Latif <MohamedLatif@my.uopeople.edu>
2025-10-29 20:13:52 +0300

    refactor: update cart routes

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,28 +1,21 @@
-import express, { Response } from "express";
-import { getCartUser, addItemToCart } from "../services/cartService";
+import express from "express";
+import { getCart, addItem } from "../controllers/cartController";
 import validateJWT from "../middlewares/validateJWT";
-import { ExtendsRequest } from "../types/extendsRequest";
 
 const router = express.Router();
 
-// get cart
-router.get("/", validateJWT, async (req: ExtendsRequest, res: Response) => {
-  const userId = req?.user?._id;
-  const cart = await getCartUser({ userId });
-  res.status(200).json(cart);
-});
+/**
+ * GET /cart
+ * Get user's cart
+ * @access Private (Requires JWT)
+ */
+router.get("/", validateJWT, getCart);
 
-//
-router.post(
-  "/items",
-  validateJWT,
-  async (req: ExtendsRequest, res: Response) => {
-    const userId = req?.user?._id;
-    const { productId, quantity } = req.body;
-
-    const response = await addItemToCart({ userId, productId, quantity });
-    res.status(response.statusCode || 200).json(response.data);
-  }
-);
+/**
+ * POST /cart/items
+ * Add item to cart
+ * @access Private (Requires JWT)
+ */
+router.post("/items", validateJWT, addItem);
 
 export default router;
